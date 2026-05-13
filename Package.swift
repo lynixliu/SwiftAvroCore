26b0d157e3ccb05b2fdabe5bb5bf8ed996765ff5
Yang Liu <liuyangsmail@gmail.com>
2026-05-14 00:14:42 +1200

    #52 remove cluster support from Avro package

--- Package.swift
+++ Package.swift
@@ -8,15 +8,13 @@ let package = Package(
         .iOS(.v18)
     ],
     products: [
-        .library(name: "SwiftAvroCore",    targets: ["SwiftAvroCore"]),
-        .library(name: "SwiftAvroRpc",     targets: ["SwiftAvroRpc"]),
-        .library(name: "SwiftAvroCluster", targets: ["SwiftAvroCluster"]),
+        .library(name: "SwiftAvroCore", targets: ["SwiftAvroCore"]),
+        .library(name: "SwiftAvroRpc",  targets: ["SwiftAvroRpc"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git",               from: "2.82.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git",           from: "2.28.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git",            from: "3.0.0"),
-        .package(url: "https://github.com/apple/swift-distributed-actors.git",branch: "main"),
+        .package(url: "https://github.com/apple/swift-nio.git",     from: "2.82.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.28.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git",  from: "3.0.0"),
     ],
     targets: [
         // MARK: - SwiftAvroCore
@@ -44,19 +42,5 @@ let package = Package(
             swiftSettings: [.swiftLanguageMode(.v6)]
         ),
 
-        // MARK: - SwiftAvroCluster
-        .target(
-            name: "SwiftAvroCluster",
-            dependencies: [
-                "SwiftAvroRpc",
-                .product(name: "DistributedCluster", package: "swift-distributed-actors"),
-            ],
-            swiftSettings: [.swiftLanguageMode(.v6)]
-        ),
-        .testTarget(
-            name: "SwiftAvroClusterTests",
-            dependencies: ["SwiftAvroCluster"],
-            swiftSettings: [.swiftLanguageMode(.v6)]
-        ),
     ]
 )
